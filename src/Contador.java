a832303d640e7d279eb28cccee247900d62a5a7a
bruno <bp.nascimento12@gmail.com>
2025-07-24 15:27:36 -0300

    Adição do gitignore

--- src/Contador.java
+++ src/Contador.java
@@ -13,23 +13,19 @@ public class Contador {
 		int parametroDois = terminal.nextInt();
 		terminal.close();
 		try {
-			//chamando o método contendo a lógica de contagem
 			contar(parametroUm, parametroDois);
 		
-		}catch (ParametrosInvalidosException exception) {
-            //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+		} catch (ParametrosInvalidosException exception) {      
             System.out.println("O segundo parâmetro deve ser maior que o primeiro");
 		}
 		
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
-        //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
         if (parametroUm > parametroDois) {
             throw new ParametrosInvalidosException("O primeiro parâmetro não pode ser maior que o segundo parâmetro !");
         }
 		
 		int contagem = parametroDois - parametroUm;
-        //realizar o for para imprimir os números com base na variável contagem
         for (;contagem > 0; contagem--){
             System.out.printf("Número: %s \n", contagem);
         }
